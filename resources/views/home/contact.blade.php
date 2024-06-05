007593e4be10942664b67167f32d12cedd04ee33
Hanvin Aditya <hanvinadityaa@gmail.com>
2024-06-05 10:18:47 +0700

    ubah tempalte

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -1,9 +1,7 @@
 <section class="contact_section ">
     <div class="container px-0">
         <div class="heading_container ">
-            <h2 class="">
-                Contact Us
-            </h2>
+            <h2 class="">Location & Reservation</h2>
         </div>
     </div>
     <div class="container container-bg">
@@ -11,7 +9,7 @@
             <div class="col-lg-7 col-md-6 px-0">
                 <div class="map_container">
                     <div class="map-responsive">
-                        <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France" width="600" height="300" frameborder="0" style="border:0; width: 100%; height:100%" allowfullscreen></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.7685110796447!2d107.47768160209213!3d-6.42378161234448!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69731acf59a4b1%3A0xb430ac50e60e6106!2sKopi%20d&#39;kebon%20by%20SCmm!5e0!3m2!1sen!2sid!4v1717555948461!5m2!1sen!2sid" width="600" height="300" frameborder="0" style="border:0; width: 100%; height:100%" allowfullscreen></iframe>
                     </div>
                 </div>
             </div>
